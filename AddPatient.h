e1cead8fc0454feb819eedcfd90d34368145d5e1
Syed M.Jafar Ali Naqvi <103287122+Sniper512@users.noreply.github.com>
2022-12-01 22:27:06 +0500

    Main C file and 2 header files comitted
    Main C file which controls all the processing named Patient_Record.c is committed. Also two header files AddPatient.h and Delete_Patient.h are committed. They contain functions to add new record and delete any record in file respectively.

---
+++ AddPatient.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#define size 30
+extern unsigned int total_record;
+struct Patient
+{
+    char id[size];
+    char name[size];
+    char cnic[size];
+    char phone[size];
+    char disease[size];
+    char isadmitted[size];
+};
+void spaces(char array[])
+{
+    int length = strlen(array);
+    for (int i = length + 1; i < 30; i++)
+    {
+        array[i] = ' ';
+    }
+}
+void AddPatient()
+{
+    FILE *ptr = NULL;
+    struct Patient patient;
+    ptr = fopen("Patient_Records.txt", "a");
+    if (ptr == NULL)
+    {
+        printf("Could not open file.");
+    }
+    else
+    {
+        printf("Enter Patient id: ");
+        gets(patient.id);
+        spaces(patient.id);
+        printf("Enter Patient name: ");
+        gets(patient.name);
+        spaces(patient.name);
+        printf("Enter Patient cnic: ");
+        gets(patient.cnic);
+        spaces(patient.cnic);
+        printf("Enter Patient Phone: ");
+        gets(patient.phone);
+        spaces(patient.phone);
+        printf("Enter Patient's disease: ");
+        gets(patient.disease);
+        spaces(patient.disease);
+        printf("Enter if patient is admitted: ");
+        gets(patient.isadmitted);
+        spaces(patient.isadmitted);
+        fwrite(&patient, sizeof(struct Patient), 1, ptr);
+        fclose(ptr);
+        total_record++;
+    }
+}
---
+++ Patient_Record.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "AddPatient.h"
+#include "Delete_Patient.h"
+unsigned int total_record=0;
+int main()
+{
+    printf("Welcome to Hospital Patient Record System!\n\n");
+    int flag = 1;
+    int choice = 0;
+    while (flag)
+    {
+        printf("1. Add Patient\n");
+        printf("2. Delete Patient\n");
+        printf("3. Update Patient\n");
+        printf("4. Search Patient\n");
+        printf("5. Display all Patients\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        int hold = 0;
+        while ((hold = getchar() != '\n' && hold != EOF))
+            ;
+        switch (choice)
+        {
+        case 1:
+            AddPatient();
+            break;
+        case 2:
+            DeletePatient();
+            break;
+        default:
+            flag = 0;
+            break;
+        }
+    }
+
+    return 0;
+}
